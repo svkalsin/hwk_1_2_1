aa42ccdcce84ec9b303f985f3030324a3064de6c
Sergey Kalsin <ksvk4@mail.ru>
2020-11-01 17:12:40 +0300

    Доработка

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,8 @@ fun main() {
     val amount = 500000
     val minCommission =3500
     val percentCommission = 0.0075f
-
-    val commission = if (amount*percentCommission < minCommission) minCommission/100 else amount*percentCommission/100
+    val amountPercentCommission = amount*percentCommission
+    val commission = if (amountPercentCommission < minCommission) minCommission/100 else amountPercentCommission/100
 
     print("Комиссия составит: $commission рублей")
 }
